400431519e772ef253ff1d0055bd0a81523cd6f1
Anna Savchuk <anna060411@gmail.com>
2021-12-13 21:32:48 +0200

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+// const { program } = require("commander");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const contactsOperations = require("./db");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
+      break;
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      console.log(contact);
+      break;
+    case "add":
+      const newContact = await contactsOperations.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+    case "update":
+      const updateContact = await contactsOperations.updateById({
+        id,
+        name,
+        email,
+        phone,
+      });
+      console.log(updateContact);
+      break;
+    case "remove":
+      const removeContact = await contactsOperations.removeContact(id);
+      console.log(removeContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
+
+invokeAction(argv);
